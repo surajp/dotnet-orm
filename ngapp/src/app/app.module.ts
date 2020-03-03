cf3eddee82fcd45da9dabf5e8188e634b6e2823b
Suraj Pillai <suraj.pillai@vertexcs.com>
2020-03-02 22:47:08 -0500

    Make ng app work

--- ngapp/src/app/app.module.ts
+++ ngapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstCompComponent } from './first-comp/first-comp.component';
+import { SecondCompComponent } from './second-comp/second-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstCompComponent,
+    SecondCompComponent
   ],
   imports: [
     BrowserModule,
